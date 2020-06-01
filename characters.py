e4453ac0ffb0523f6e080a4419541709e6c03751
Mitchell <45104653+mitchellmui17@users.noreply.github.com>
2020-06-01 17:39:42 -0400

    Initial commit

---
+++ characters.py
@@ -0,0 +1,33 @@
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+
+# Window
+top = Tk(className='LogIn')
+top.geometry("300x300")
+
+# Username Information
+user = Label(top, text="User Name:").grid(row=0)
+password = Label(top, text="Password:").grid(row=1)
+e1 = Entry(top)
+e2 = Entry(top)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+
+def textFile():
+    file = open("beautbst.txt", "r")
+    print(file.read())
+    messagebox.showinfo("Beauty and the Beast", "Printed Successfully!")
+
+
+def check():
+    if e1.get() == "admin" and e2.get() == "123":
+        print("test")
+        textFile()
+    else:
+        print("Incorrect user / password")
+
+
+enter = tkinter.Button(top, text="login", command=check).grid(row=2)
+top.mainloop()
